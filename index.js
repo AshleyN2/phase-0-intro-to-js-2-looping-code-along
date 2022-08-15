a8eba42bb18db0d9f9dc0dcfd2a08efea93b297b
Ashley <ashleynjeri7@gmail.com>
2022-08-15 13:57:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = ['Guadalupe', 'Ollie', 'Aki'];
+const event = 'surprise';
+
+function writeCards(){
+    const customMessages = [];
+    for (let j = 0; j < names.length; j++) {
+        customMessages.push(`Thank you, ${names[j]}, for the wonderful surprise gift!`);
+    }
+    return customMessages;
+}
+
+function countDown(){
+    let countDown = 11;
+    while (countDown--)  {
+        console.log(countDown);
+    }
+}
